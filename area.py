0f8ab66a97f106c5564bcf374b08a3448bf7d39b
root <root@localhost.localdomain>
2020-07-13 11:58:52 +0530

    my first commit

---
+++ area.py
@@ -0,0 +1,7 @@
+length = float(input('Please Enter the Length of a Triangle: '))
+width = float(input('Please Enter the Width of a Triangle: '))
+
+# calculate the area
+area = length * width
+
+print("The Area of a Rectangle using", length, "and", width, " = ", area)
